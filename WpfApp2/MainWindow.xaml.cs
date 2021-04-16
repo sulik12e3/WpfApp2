c2ec59cb04987b3719df9782b6e67ff7832ce3f7
sulik12e3 <sulik.gasanov.95@mail.ru>
2021-04-16 23:51:51 +0300

    Initial commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        easyHiretapesEntities db = new easyHiretapesEntities();
+        
+        void Update()
+        {
+            dg.ItemsSource = db.Videotapes.ToList();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dg.ItemsSource = Connectdb.db.Videotapes.ToList();
+        }
+
+       
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+             Window1 s = new Window1();
+              s.Show();
+          
+        }
+
+        private void delete_Click(object sender, RoutedEventArgs e)
+        {
+            var currenttapes = dg.SelectedItem as Videotapes;
+            db.Videotapes.Remove(currenttapes);
+            db.SaveChanges();
+            Update();
+        }
+
+        private void Change_Click(object sender, RoutedEventArgs e)
+        {
+            Connectdb.db.SaveChanges();
+        }
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+          ///  cmbgenre.ItemsSource = Connectdb.db.Genre.Select(itemG => itemG.Name).ToList();
+
+        }
+   
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Videotapes vs = new Videotapes();
+                vs.NameTapes = txbNameT.Text;
+                vs.Quantity = Convert.ToInt32(Txbquantity);
+                vs.QuantityAviable = Convert.ToInt32(TXBAVIABLE);
+                vs.Genre.Name = txbgenre.Text;
+                
+                Connectdb.db.SaveChanges();
+            }
+            catch
+            {
+                MessageBox.Show("Добавление прошло успешно", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+    }
+}
